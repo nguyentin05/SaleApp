1535a11dcf64fd10366329666ce35107542555b5
Trong Tin <tin.nguyen.cs05@gmail.com>
2025-10-27 00:02:59 +0700

    split htmlfiles and modify jsonfiles

--- app/index.py
+++ app/index.py
@@ -9,14 +9,18 @@ app = Flask(__name__)
 def index():
     q= request.args.get("q")
     cate_id = request.args.get("cate_id")
-    cates = dao.load_categories()
     products = dao.load_products(q=q, cate_id=cate_id)
-    return render_template("index.html",cates = cates,products=products)
+    return render_template("index.html",products=products)
 
 @app.route("/products/<int:id>")
 def detail(id):
-    cates = dao.load_categories()
-    return render_template("product_detail.html",p=dao.get_product_by_id(id),cates=cates)
+    return render_template("product_detail.html",p=dao.get_product_by_id(id))
+
+@app.context_processor
+def common_attribute():
+    return {
+        "cates": dao.load_categories()
+    }
 
 if __name__ == "__main__":
     with app.app_context():
